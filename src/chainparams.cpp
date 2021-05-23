d3bd490a699d7cb7921affc84daa4fd8dbebf99b
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-05-23 02:48:28 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -2,6 +2,8 @@
 // Copyright (c) 2009-2018 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#include <stdio.h>
+#include <inttypes.h>
 
 #include <chainparams.h>
 #include <arith_uint256.h>
